0b4bff6132c301b90725d61db92abad17b449859
gpandres <hola@andresgp.dev>
2025-10-21 11:22:11 +0000

    Datos modificados

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php
-const base_url = "http://localhost/biblio/";
+const base_url = "http://biblioteca.andres.org/";
 const host = "localhost";
-const user = "root";
-const pass = "";
+const user = "admin";
+const pass = "Usuario.25";
 const db = "biblioteca";
 const charset = "charset=utf8";
 ?>
